a41a40800149aa3293d9f04b6edff709b0b4f714
Steffen Beginn <115171161+sBeginn@users.noreply.github.com>
2023-09-24 12:28:07 +0200

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,1 +1,111 @@
-import streamlit as stimport cv2import pandas as pdimport matplotlib.pyplot as plt# Titel deiner Anwendungst.title('Fitnessübungen mit Streamlit')# Benutzerfreundliche Meldungenst.write("Wähle eine Übung aus und entscheide, ob du sie live mit der Webcam durchführen oder ein Video hochladen möchtest.")# Übungen verwaltenexercises = {    'Übung 1': 'Hier kommt die Beschreibung für Übung 1.',    'Übung 2': 'Hier kommt die Beschreibung für Übung 2.',    'Übung 3': 'Hier kommt die Beschreibung für Übung 3.'}selected_exercise = st.selectbox('Übung auswählen', list(exercises.keys()))# Option zur Auswahl von Webcam oder Videolive_or_video = st.radio('Live mit Webcam oder Video', ['Webcam', 'Video'])# Datenframe zur Verfolgung der Leistungperformance_data = pd.DataFrame(columns=['Übung', 'Dauer (s)', 'Wiederholungen'])# Webcam-Integrationif live_or_video == 'Webcam':    st.write('Du hast "Webcam" ausgewählt. Bitte erlaube den Zugriff auf deine Webcam.')    # Video-Stream von der Webcam anzeigen    cap = cv2.VideoCapture(0)    if st.button('Starte die Übung'):        st.session_state.time = st.session_state.time or st.empty()        st.session_state.time.empty()        st.write(f'Du führst jetzt die Übung "{selected_exercise}" live mit der Webcam durch.')        st.write('Stoppe die Übung, wenn du fertig bist.')        start_time = st.session_state.time.time()        repetitions = 0        while True:            ret, frame = cap.read()            if not ret:                break            st.image(frame, channels='BGR')            if st.button('Fertig'):                end_time = st.session_state.time.time()                duration = (end_time - start_time).total_seconds()                repetitions += 1                st.write(f'Dauer der letzten Wiederholung: {duration:.2f} s')                st.write(f'Gesamtanzahl der Wiederholungen: {repetitions}')                performance_data = performance_data.append({'Übung': selected_exercise, 'Dauer (s)': duration, 'Wiederholungen': repetitions}, ignore_index=True)                st.write('Leistungsdaten:')                st.write(performance_data)                start_time = st.session_state.time.time()        cap.release()# Video-Upload-Optionelif live_or_video == 'Video':    st.write('Du hast "Video" ausgewählt. Bitte lade ein Video im MP4- oder AVI-Format hoch.')    # Video-Upload    uploaded_file = st.file_uploader('Video auswählen', type=['mp4', 'avi'])    if uploaded_file is not None:        st.write(f'Du führst jetzt die Übung "{selected_exercise}" mit dem hochgeladenen Video durch.')        st.video(uploaded_file)# Übungsbeschreibung anzeigenif selected_exercise in exercises:    st.write(f'**Übungsbeschreibung für "{selected_exercise}":**')    st.write(exercises[selected_exercise])# Fortschrittsdiagramm anzeigenif not performance_data.empty:    st.write('**Fortschrittsdiagramm**')    fig, ax = plt.subplots()    for exercise, data in performance_data.groupby('Übung'):        ax.plot(data.index, data['Dauer (s)'], marker='o', label=exercise)    ax.set_xlabel('Durchlauf')    ax.set_ylabel('Dauer (s)')    ax.set_title('Leistungsfortschritt')    ax.legend()    st.pyplot(fig)
+import streamlit as st
+import subprocess
+import MPT_pushup_video 
+import MPT_squat_video
+
+# Streamlit-Anmeldeseite
+st.title('Anmeldung erforderlich')
+
+# Eingabefelder für Benutzername und Passwort
+username = st.text_input('Benutzername:')
+password = st.text_input('Passwort:', type='password')
+
+# Benutzername und Passwort zum Vergleich
+valid_username = "FC"
+valid_password = "F"
+
+# Flag, um anzuzeigen, ob der Benutzer angemeldet ist
+is_auth = username == valid_username and password == valid_password
+
+if is_auth:
+    # Display the username and password as text
+    st.write(f'Eingeloggt als Benutzer: {username}')
+    st.write('Passwort: ********')  # Display password as asterisks for security
+
+    # Streamlit-Sidebar für die Navigation
+    st.sidebar.title('Navigation')
+    selected_page = st.sidebar.radio('Wähle eine Seite', ['Dashboard', 'Übungen verwalten'])
+
+    if selected_page == 'Dashboard':
+        # Dashboard-Bereich
+        st.title('Fitnessübungen')
+
+        # Benutzerfreundliche Meldungen
+        st.write("Wähle eine Übung aus und entscheide, ob du sie live mit der Webcam durchführen oder ein Video hochladen möchtest.")
+
+        # Übungen verwalten
+        exercises = {
+            'Liegestützen': 'Hier kommt die Beschreibung für Liegestützen.',
+            'Kniebeugen': 'Hier kommt die Beschreibung für Kniebeugen.',
+            'Übung 3': 'Hier kommt die Beschreibung für Übung 3.'
+        }
+        
+        # Create a placeholder for the video analysis results
+        result_placeholder = st.empty()
+
+        # Selected exercise and live_or_video option
+        selected_exercise = st.selectbox('Übung auswählen', list(exercises.keys()))
+        live_or_video = st.radio('Live mit Webcam oder Video', ['Webcam', 'Video'])
+
+        if selected_exercise == 'Liegestützen' and live_or_video == 'Webcam':
+            st.write('Du hast "Webcam" ausgewählt. Bitte erlaube den Zugriff auf deine Webcam.')
+            if st.button('Starte mit den Liegestützen'):
+                subprocess.run(['python', r'C:\Users\Schule\Desktop\MPT_Project\MPT_pushup_webcam.py'])
+
+        if selected_exercise == 'Liegestützen' and live_or_video == 'Video':
+            st.write('Du hast "Video" ausgewählt. Lade ein Video hoch:')
+            uploaded_file = st.file_uploader("Video-Datei hochladen", type=["mp4", "avi"])
+            
+            if uploaded_file is not None:
+                # Save the uploaded video to a temporary file
+                with open("temp_video.mp4", "wb") as f:
+                    f.write(uploaded_file.read())
+        
+                # Call the main function from the MPT_pushup_video module
+                video_path = "temp_video.mp4"  # The path to the uploaded video
+                result = MPT_pushup_video.main(video_path, shoulder_threshold_meters=0.2, fingertips_threshold_pixels=50)
+
+                # Display the analysis results
+                st.write(f"Overall Performance Score: {result['overall_score']:.2f}%")
+                st.write(result['shoulder_feedback'])
+                st.write(result['hand_feedback'])
+
+        if selected_exercise == 'Kniebeugen' and live_or_video == 'Webcam':
+            st.write('Du hast "Webcam" ausgewählt. Bitte erlaube den Zugriff auf deine Webcam.')
+            if st.button('Starte mit den Kniebeugen'):
+                subprocess.run(['python', r'C:\Users\Schule\Desktop\MPT_Project\MPT_squat_webcam.py'])
+
+        if selected_exercise == 'Kniebeugen' and live_or_video == 'Video':
+            st.write('Du hast "Video" ausgewählt. Lade ein Video hoch:')
+            uploaded_file = st.file_uploader("Video-Datei hochladen", type=["mp4", "avi"])
+            
+            if uploaded_file is not None:
+                # Save the uploaded video to a temporary file
+                with open("temp_video.mp4", "wb") as f:
+                    f.write(uploaded_file.read())
+        
+                # Call the main function from the MPT_pushup_video module
+                video_path = "temp_video.mp4"  # The path to the uploaded video
+                result = MPT_squat_video.main(video_path, shoulder_threshold_meters=0.2, toes_threshold_degrees=15)
+
+                # Display the analysis results
+                st.write(f"Overall Performance Score: {result['overall_score']:.2f}%")
+                st.write(result['shoulder_feedback'])
+                st.write(result['knee_feedback'])
+                st.write(result['toes_feedback'])
+
+    elif selected_page == 'Übungen verwalten':
+        # Seite für die Verwaltung der Übungen
+        st.title('Übungen verwalten')
+
+        # Hinzufügen von Übungen
+        new_exercise_name = st.text_input('Name der neuen Übung:')
+        new_exercise_description = st.text_area('Beschreibung der neuen Übung:')
+        if st.button('Übung hinzufügen'):
+            if new_exercise_name and new_exercise_description:
+                exercises[new_exercise_name] = new_exercise_description
+                st.success(f'Übung "{new_exercise_name}" wurde hinzugefügt.')
+
+        # Liste der vorhandenen Übungen anzeigen
+        st.write('**Liste der vorhandenen Übungen:**')
+        st.write(exercises)
